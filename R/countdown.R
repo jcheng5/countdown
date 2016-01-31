b45d0569ed7cb68a06d870bdeae7759e055efce8
Joe Cheng <joe@rstudio.com>
2016-01-31 00:28:35 -0800

    Initial commit

---
+++ R/countdown.R
@@ -0,0 +1,31 @@
+#' @import htmltools lubridate
+#' @export
+countdown_timer <- function(seconds) {
+  td <- seconds_to_period(seconds)
+  timeString <- sprintf('%02d:%02d:%02d:%02d', day(td), td@hour, minute(td), second(td))
+  timeString <- sub("^(00:){0,2}0?", "", timeString, perl = TRUE)
+
+  deps <- list(
+    htmlDependency("jquery", "1.12.0",
+      src = system.file("jquery", package="countdown"),
+      script = "jquery-1.12.0.min.js"
+    ),
+    htmlDependency("countdown_timer", packageVersion("countdown"),
+      src = system.file("assets", package="countdown"),
+      script = "countdown.js",
+      stylesheet = "countdown.css"
+    )
+  )
+  attachDependencies(
+    withTags(
+      div(class="countdown-timer", `data-timespan`=as.character(seconds),
+        span(class="countdown-timer-time", timeString),
+        br(),
+        button(class="countdown-timer-start", type="button", "Start"),
+        button(class="countdown-timer-stop", type="button", "Stop"),
+        button(class="countdown-timer-reset", type="button", "Reset")
+      )
+    ),
+    deps
+  )
+}
